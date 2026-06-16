f31b98bef414c22ae49e8acfa17d31883c7a1f5d
karstipt <karstipt@outlook.de>
2026-06-16 22:35:00 +0200

    Update BootScene.ts

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -6,6 +6,11 @@ export class BootScene extends Phaser.Scene {
   preload() {
     this.load.image('kristall', 'assets/sprites/buildings/kristall.png');
     this.load.image('zaun',     'assets/sprites/buildings/zaun.png');
+    this.load.image('icon-holz',      'assets/sprites/ui/holz.svg');
+    this.load.image('icon-erz',       'assets/sprites/ui/erz.svg');
+    this.load.image('icon-metall',    'assets/sprites/ui/metall.svg');
+    this.load.image('icon-nahrung',   'assets/sprites/ui/nahrung.svg');
+    this.load.image('icon-diamanten', 'assets/sprites/ui/diamanten.svg');
   }
 
   create() {
