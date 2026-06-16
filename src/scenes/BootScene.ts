89126d15c42b0e351d2a8da608e60b660faea99c
karstipt <karstipt@outlook.de>
2026-06-16 20:30:35 +0200

    Update BootScene.ts

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -4,9 +4,8 @@ export class BootScene extends Phaser.Scene {
   constructor() { super({ key: 'BootScene' }); }
 
   preload() {
-    // TODO: Alle Assets hier laden
-    // this.load.image('grass', 'assets/tilemaps/grass.png');
-    // this.load.spritesheet('goblin', 'assets/sprites/units/goblin.png', { frameWidth: 64, frameHeight: 64 });
+    this.load.image('kristall', 'assets/sprites/buildings/kristall.png');
+    this.load.image('zaun',     'assets/sprites/buildings/zaun.png');
   }
 
   create() {
